20c41b55168b0bb6c0da0c3d3c81b4a8a9628993
JosephRivera10 <joseph.rivera.09@gmail.com>
2018-03-02 12:30:14 -0600

    burgers

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,38 @@
+$(function(){
+	$(".eatBurger").on("click", function(event) {
+		var id = $(this).data("id");
+		var newEat = $(this).data("neweat");
+
+		var newEatState = {
+			devoured: newEat
+		};
+
+		$.ajax("/api/burgers/" + id, {
+			type: "PUT",
+			data: newEatState
+		}).then(
+		function() {
+			console.log("changed eat to" + newEat);
+			location.reload();
+		});
+	});
+
+	$(".create-form").on("submit", function(event) {
+		event.preventDefault();
+
+		var newBurger = {
+			burger_name: $("#bu").val().trim()
+			// devoured: $("[name=devoured]:checked").val().trim()
+		};
+
+		$.ajax("/api/burgers", {
+			type: "POST",
+			data: newBurger
+		}).then(
+		function() {
+			console.log("created new burger");
+
+			location.reload();
+		});
+	});
+});
